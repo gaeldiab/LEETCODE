eb9255763b98888cbd1dd255e00ab9e5812374b4
Gael <gaeldiab1@gmail.com>
2023-07-18 23:19:35 -0400

    problem 2

---
+++ 2_add_two_numbers.cpp
@@ -0,0 +1,51 @@
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution 
+{
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+        ListNode *result = new ListNode(0);
+        int carry = 0;
+        ListNode *head = result;
+        while(l1 && l2)
+        {
+            int sum  = l1->val + l2->val + carry;
+            carry = sum/10;
+            result->next = new ListNode(sum%10);
+            result = result->next;
+            l1 = l1->next;
+            l2= l2->next;
+
+        }
+        while(l1)
+        {
+            int sum  = l1->val+carry;
+            carry = sum/10;
+            result->next = new ListNode(sum%10);
+            result = result->next;
+            l1 = l1->next;
+
+        }
+        while(l2)
+        {
+            int sum  = l2->val + carry;
+            carry = sum/10;
+            result->next = new ListNode(sum%10);
+            result = result->next;
+            l2 = l2->next;
+
+        }
+        if(carry)
+        {
+            result->next= new ListNode(carry);
+        }
+        return head->next;
+    }
+};
