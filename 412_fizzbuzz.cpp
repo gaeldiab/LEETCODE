6b34dd7f88dbb5c9f812dbf3646e2bf5689d3588
Gael <gaeldiab1@gmail.com>
2023-07-19 21:10:25 -0400

    412

---
+++ 412_fizzbuzz.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+class Solution 
+{
+public:
+    std::vector<std::string> fizzBuzz(int n) 
+    {
+        std::vector<std::string> answer(n,"");
+        for (int i = 1; i<n+1;i++)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                answer[i-1] = "FizzBuzz";
+            }
+            else if(i%3==0)
+            {
+                answer[i-1]= "Fizz";
+            }
+            else if(i%5==0)
+            {
+                answer[i-1]="Buzz";
+            }
+            else
+            {
+                answer[i-1]= std::to_string(i);
+            }
+        }
+        return answer;
+    }
+};
